d907f7c0f1be555a42abd75c87131c4ee51df127
Katie A <katie.t.abraham@gmail.com>
2021-06-21 23:34:41 -0400

    adding promise js

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,3 +1,4 @@
+import Vue from 'vue';
 import '../theme';
 import getAllProducts from '../graphql/collection-starter-code';
 
@@ -5,3 +6,27 @@ import getAllProducts from '../graphql/collection-starter-code';
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
 
 // Your Code Here
+
+
+
+export default {
+    data: () => ({ products: [] }),
+    methods: {
+        beforeCreate() {
+            console.log(getAllProducts('test-collection'));
+            return this.$api.get('products')
+            .then(r => {
+                this.products = r.data
+            })
+            .catch(error => {
+                this.$notify.error('Something is amiss...')
+                throw error
+            })
+        },
+        async asyncAction() {
+            await this.performAsyncMethod(
+                await this.$store.dispatch('someAction')
+            )
+        }
+    }
+}
