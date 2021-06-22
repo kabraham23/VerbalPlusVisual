b77e0bbd5be0197c0bdb5f055d6a27cc4b5a079d
Katie A <katie.t.abraham@gmail.com>
2021-06-22 10:00:16 -0400

    final changes

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import '../theme';
 import getAllProducts from '../graphql/collection-starter-code';
+import { render } from 'node-sass';
 
 
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
@@ -10,22 +11,29 @@ import getAllProducts from '../graphql/collection-starter-code';
 
 
 export default {
-    data: () => ({ products: [] }),
+
+    state = {
+        data: {}
+    },
+
+    getData = () => ({ products: [] }),
     methods: {
         beforeCreate() {
-            console.log(getAllProducts('test-collection'));
-            return this.getAllProducts('test-collection')
-            .then(r => {
-                this.products = r.data
-            })
-            .catch(error => {
-                this.$notify.error('Something is amiss...')
-                throw error
-            })
+            fetch(getAllProducts('test-collection'))
+            .then(data => data.json())
+            .then(data => this.setState({data}))
         },
-        async asyncAction() {
-            await this.performAsyncMethod(
-                await this.$store.dispatch('someAction')
+        render() {
+            return (
+                <div>
+                    {Object.keys(this.state.data).length && this.state.data.products.map((data) => {
+                        return (
+                            <div id="product-card">
+                                
+                            </div>
+                        )
+                    })}
+                </div>
             )
         }
     }
